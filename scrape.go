090b23a4ad8d87f48031a07d7d5faec39c93fcb2
alexa-whitney <alexawhitney@gmail.com>
2024-04-10 22:59:26 -0600

    Scraping Requirements Completed

--- scrape.go
+++ scrape.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+// Movie holds the data about a movie
+type Movie struct {
+	Title string
+}
+
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	// Counter variable to keep track of the number of movies scraped
+	var movieCount int
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// OnHTML callback for the movie title based on the CSS selector
+	c.OnHTML("h3.ipc-title__text", func(e *colly.HTMLElement) {
+		// Stop scraping when we've collected the top 10 movies
+		if movieCount >= 10 {
+			return
+		}
+
+		// Extract the movie title text
+		movieTitle := e.Text
+
+		// This checks if the title includes a ranking number like "1. " and removes it
+		parts := strings.SplitN(movieTitle, " ", 2)
+		if len(parts) > 1 {
+			movieTitle = parts[1]
+		}
+
+		movie := Movie{
+			Title: movieTitle,
+		}
+
+		// Increment the movie count
+		movieCount++
+
+		// Print the movie title
+		fmt.Printf("Top %d Movie: %s\n", movieCount, movie.Title)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +51,6 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping on IMDb Top 250 Movies page
+	c.Visit("https://www.imdb.com/chart/top/")
 }
